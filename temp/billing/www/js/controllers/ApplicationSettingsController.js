d87a277a6393c10620a1014bb90892390ee692b7
vova <voevidka.vova.v@gmail.com>
2014-07-29 01:29:13 +0300

    - fix bug with app settings;

--- temp/billing/www/js/controllers/ApplicationSettingsController.js
+++ temp/billing/www/js/controllers/ApplicationSettingsController.js
@@ -1,9 +1,9 @@
 var app = window.app_controllers;
 app.controller('ApplicationSettingsCtrl', function($scope, API_URL) {
 	window.localStorage['API_URL'] = API_URL.value;
-    $scope.apiUrl = angular.copy(API_URL.value);
+    $scope.apiUrl = {value: angular.copy(API_URL.value)};
     $scope.saveApiUrl = function() {
-        API_URL.value = $scope.apiUrl;
+        API_URL.value = $scope.apiUrl.value;
         window.localStorage['API_URL'] = API_URL.value;
     };
 });
